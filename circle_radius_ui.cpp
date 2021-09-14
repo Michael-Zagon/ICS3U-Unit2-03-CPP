e329f5fe2d9dfdcc26bf841f03bd6d0d4418e06c
Ubuntu <ubuntu@ip-172-31-7-8.ec2.internal>
2021-09-14 22:26:33 +0000

    circle radius calculator cpp ui

---
+++ circle_radius_ui.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 Michael Zagon All rights reserved
+
+// Created by: Michael Zagon
+// Created on: September 2021
+// This program calculates the circumference of a circle with user input
+
+#include <iostream>
+
+int main() {
+    // this function calculates the circumference of a circle
+    const double TAU = 6.28;
+    int radius;
+    double circumference;
+    
+    // input
+    std::cout << "Enter radius of the circle (mm): ";
+    std::cin >> radius;
+    
+    // process
+    circumference = TAU*radius;
+    
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "Circumference is " << circumference << " mm" << std::endl;
+}
